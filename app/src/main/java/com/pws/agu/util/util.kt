29310e59091723d610a7af62197499205faf1683
PwS <pawanasutha02@gmail.com>
2021-01-09 16:52:30 +0700

    Remove Token

--- app/src/main/java/com/pws/agu/util/util.kt
+++ app/src/main/java/com/pws/agu/util/util.kt
@@ -1,6 +1,5 @@
 package com.pws.agu.util
 
 enum class Util(val util: String) {
-    url("https://api.github.com/"),
-    pAToken("f73f3785e93b245863245ebb558a61857e786776")
+    url("https://api.github.com"),
 }
